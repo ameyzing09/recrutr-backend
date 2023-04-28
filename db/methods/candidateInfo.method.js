b356d5578021a1d53acf18b660b270d18253ffc5
Amey Kode <55131226+ameyzing09@users.noreply.github.com>
2023-04-28 10:39:09 +0530

    Add candidate api (#9)
    * Added addCandidateInfo
    
    * added dbmigrate which is working (#6)
    
    * made some changes in package.json
    
    * Add candidate api (#8)
    
    * Add candidate api (#7)
    
    * Added addCandidateInfo
    
    * added dbmigrate which is working (#6)
    
    * made some changes in package.json
    
    * update candidate api

--- db/methods/candidateInfo.method.js
+++ db/methods/candidateInfo.method.js
@@ -6,5 +6,6 @@ candidateInfoModel.belongsTo(userModel, { foreignKey: 'user_id' });
 export const create = async payload => await candidateInfoModel.create(payload);
 export const fetchOne = async payload => await candidateInfoModel.findOne(payload);
 export const fetchAll = async payload => await candidateInfoModel.findAll(payload);
+export const fetchAndCountAll = async options => await candidateInfoModel.findAndCountAll(options);
 export const update = async (payload, options) => candidateInfoModel.update(payload, options);
 export const erase = async options => candidateInfoModel.destroy(options);
