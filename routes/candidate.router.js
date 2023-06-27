67864c5300ed57918dfaebc431aee55a66c0abfc
Amey Kode <ameykode2001@gmail.com>
2023-06-27 20:30:00 +0530

    Temp commit

--- routes/candidate.router.js
+++ routes/candidate.router.js
@@ -9,16 +9,40 @@ import {
   getCandidatesByPage,
   modifyCandidate,
 } from "../controller/candidate.controller.js";
+import candidateValidation from "../middleware/validations/candidateValidations.js";
+import requestValidation from "../middleware/validations/requestValidations.js";
 
 const router = Router();
 
 router.get("/", verifyToken, getCandidatesByPage);
 
-router.get("/:candidateId", verifyToken, getCandidateById);
+router.get(
+  "/:candidateId",
+  verifyToken,
+  candidateValidation.getCandidateIdQuery,
+  requestValidation,
+  getCandidateById
+);
 
-router.post("/", verifyToken, createCandidate);
+router.post(
+  "/",
+  verifyToken,
+  candidateValidation.createCandidateBody,
+  requestValidation,
+  createCandidate
+);
 
-router.put("/:candidateId", verifyToken, modifyCandidate);
+router.put(
+  "/:candidateId",
+  verifyToken,
+  [
+    candidateValidation.getCandidateIdQuery,
+    requestValidation,
+    candidateValidation.modifyCandidateBody,
+    requestValidation,
+  ],
+  modifyCandidate
+);
 
 router.delete("/:candidateId", verifyToken, deleteCandidate);
 
